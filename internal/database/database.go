06aa148e33e9fdef7f6761ba9976d490ae49d4c4
Scythe504 <hellfireprince98@gmail.com>
2025-03-22 15:25:55 +0530

    db_connection_error

--- internal/database/database.go
+++ internal/database/database.go
@@ -53,10 +53,12 @@ func New() Service {
 		return dbInstance
 	}
 
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", username, password, host, port, database, schema)
+	var connStr string
 
 	if dbUrl != "" {
 		connStr = dbUrl
+	} else {
+		connStr = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require&search_path=%s", username, password, host, port, database, schema)
 	}
 
 	db, err := sql.Open("pgx", connStr)
